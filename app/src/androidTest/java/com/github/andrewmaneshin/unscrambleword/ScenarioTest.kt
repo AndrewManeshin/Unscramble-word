8b6dcb0ea3790c3b3568f21246619031083179e8
Andrew_Maneshin <andrew.maneshin@mail.ru>
2024-08-08 20:22:55 +0300

    UG-01 my solution for UI tests

---
+++ app/src/androidTest/java/com/github/andrewmaneshin/unscrambleword/ScenarioTest.kt
@@ -0,0 +1,107 @@
+package com.github.andrewmaneshin.unscrambleword
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ScenarioTest {
+
+    private lateinit var gamePage: GamePage
+
+    @Before
+    fun setUp() {
+        gamePage = GamePage(scrumbledWord = "android")
+    }
+
+    /**
+     * UGTC-01 SkipTestCase
+     */
+    @Test
+    fun skipTest() {
+        gamePage.clickSkip()
+        gamePage().assertInitialState()
+    }
+
+    /**
+     * UGTC-02 InsufficientInputTestCase
+     */
+    @Test
+    fun InsufficientInputTest() {
+        gamePage.input("adnroi")
+        gamePage.assertInsufficientInputState()
+
+        gamePage.input("androidd")
+        gamePage.assertInsufficientInputState()
+    }
+
+    /**
+     * UGTC-03 SufficientInputTestCase
+     */
+    @Test
+    fun SufficientInputTest() {
+        gamePage.input("androit")
+        gamePage.assertSufficientInputState()
+    }
+
+    /**
+     * UGTC-04 Sufficient and Insufficient InputTestCase
+     */
+    @Test
+    fun SufficientAndInsufficientInputTest() {
+        gamePage.input("adnroi")
+        gamePage.assertInsufficientInputState()
+
+        gamePage.input("androidd")
+        gamePage.assertInsufficientInputState()
+
+        gamePage.input("androit")
+        gamePage.assertSufficientInputState()
+    }
+
+    /**
+     * UGTC-05 IncorrectTestCase
+     */
+    @Test
+    fun IncorrectTest() {
+        gamePage.input("androit")
+
+        gamePage.clickCheck()
+        gamePage.assertIncorrectState()
+    }
+
+    /**
+     * UGTC-06 SkipAfterIncorrectTestCase
+     */
+    @Test
+    fun SkipAfterIncorrectTest() {
+        gamePage.input("androit")
+
+        gamePage.clickCheck()
+        gamePage.assertIncorrectState()
+
+        gamePage.clickSkip()
+        gamePage().assertInitialState()
+    }
+
+    /**
+     * UGTC-07 CorrectAfterIncorrectTestCase
+     */
+    @Test
+    fun CorrectAfterIncorrectTest() {
+        gamePage.input("androit")
+
+        gamePage.clickCheck()
+        gamePage.assertIncorrectState()
+
+        gamePage.input("androi")
+        gamePage.assertInsufficientInputState()
+
+        gamePage.input("android")
+        gamePage.clickCheck()
+        gamePage.assertCorrectState()
+    }
+}

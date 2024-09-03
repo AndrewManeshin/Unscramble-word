07f393f51e3ce241536a751a0fb280263cc2f065
Andrew_Maneshin <andrew.maneshin@mail.ru>
2024-09-03 18:40:35 +0300

    UG-16 GameOverViewModelTest

---
+++ app/src/test/java/com/github/andrewmaneshin/unscrambleword/stats/GameOverViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.github.andrewmaneshin.unscrambleword.stats
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GameOverViewModelTest {
+
+    @Test
+    fun test() {
+        val repository = FakeRepository()
+        val viewModel = GameOverViewModel(repository = repository)
+
+        assertEquals(StatsUiState.Base(2, 3), viewModel.statsUiState())
+    }
+}
+
+private class FakeRepository : StatsRepository {
+
+    override fun stats(): Pair<Int, Int> = Pair(2, 3)
+}
